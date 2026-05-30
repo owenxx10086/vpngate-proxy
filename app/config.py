7692e0107b7c4da2c54d4e02889cb891dcaf7c13
xiaowen <62494620+xiaowen-king@users.noreply.github.com>
2026-05-30 19:17:40 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import json
 import os
 import secrets
 
-CONFIG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")
+CONFIG_PATH = "/data/config.json"
 
 DEFAULT_CONFIG = {
     "web_password": "admin",
