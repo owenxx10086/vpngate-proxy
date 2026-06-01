887377c66403f9df1c0723d376b51c13a7172cfd
xiaowen <62494620+xiaowen-king@users.noreply.github.com>
2026-06-01 14:21:41 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -21,6 +21,7 @@ DEFAULT_CONFIG = {
     "log_retention_days": 3,
     "health_check_urls": "",
     "latency_check_target": ""
+    "speedtest_url": "http://httpbin.org/bytes/1048576"
 }
 
 def load_config():
